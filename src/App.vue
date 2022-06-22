a4e0e97bc4f7fc9c2a7abdd306d8963d03b75c1d
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-22 11:07:54 -0400

    VOn-Events-Modificadors

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <list-item @increment="increment"></list-item> -->
+    <list-item></list-item>
+    {{ count1 }}
+    <!-- <input type="text" @change="text = $event">
+  <input type="text" @input="text = $event"> -->
+    <!-- <input type="text" @keyup.alt.67="increment" /> {{ count2 }} -->
+    <!-- <button @click="count1++">Click1 {{count1}}</button>
+    <button @click="increment">Click2 {{count2}}</button>
+    <button @click="increment3(2, $event)">Click3 {{count3}}</button> -->
+    <!-- <button @click.ctrl="increment">CLICK</button> -->
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListItem from "./components/ListItem.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      text: "",
+      count1: 0,
+      count2: 0,
+      count3: 0,
+    };
+  },
+
+  created(){
+    this.$root.$on('increment', this.increment);
+  },
   components: {
-    HelloWorld
-  }
-}
+    ListItem,
+  },
+  method: {
+    increment(valor,event) {
+      this.count += valor;
+      alert(event);
+
+      console.log("123123123123");
+    },
+    // increment() {
+    //   this.count1++;
+    // },
+    // increment() {
+    //   this.count2++;
+    // },
+    // increment2(val, event) {
+    //   alert(event);
+    //   this.count3 += val;
+    // },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li @click="emit">Click me</li>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  methods: {
+    emit(event) {
+      console.log(this.$root);
+      //   this.$emit("increment", 4, event);
+      this.$root.$emit("increment", 4, event); //evento global;
+    },
+  },
+};
+</script>
+
+<style></style>
