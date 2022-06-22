d7f68d34035ee0fe40ebe7deac7606d2c0980814
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-22 09:15:19 -0400

    DPC

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="font-size: 30px" :style="[color, fontSize]">diretiva v-bind</h1>
+    <!-- <h1 class="test" :class="[red, { blue: showBlue }]">Diretiva v-bind</h1> -->
+    <!-- <a :href="url">SITE</a> -->
+    <!-- <h1 v-if="state === 'loading'">LOADING</h1>
+    <h1 v-else-if="state === 'error'">ERROR</h1>
+    <h1 v-else-if="messages.length === 0">SEM DADOS</h1>
+    <h1 v-else>{{ messages }}</h1> -->
+    <!-- <h1 v-if="show">Diretiva V-IF</h1>
+    <h1 v-else-if="!show">DIRETIVA V_ELSE_IF</h1>
+    <h1 v-else>DIRETIVA V_ELSE</h1> -->
+    <!-- <h1 v-show="true">Diretiva V-SHOW</h1> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      showBlue: "true",
+      showRed: true,
+      show: false,
+      state: "loading",
+      messages: [],
+      url: "www.google.com",
+      dyamicClass: "red",
+      red: "red",
+      blue: "blue",
+      green: "green",
+      style:{ color: 'red', fontSize: '30px'},
+      color: {color: 'red'},
+      fontSize: { fontSize: '30px'},
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
