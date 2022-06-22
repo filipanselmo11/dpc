c263d950ef2c5cb05ae0ed8e199fd0248e86c6d1
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-22 10:30:23 -0400

    V-for

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <list-item v-for="(index, todo) in todos" :key="index" v-bind="todo">
+        <!-- {{todo}} -->
+      </list-item>
+      <!-- <list-item 
+        v-for="(index, todo) in todos" 
+        :key="index"
+        :todo="todo">
+      </list-item> -->
+
+      <!-- <template v-if="!todo.isCompleted">
+        <li v-for="(index, todo) in todos" :key="index">
+          {{ todo }}
+        </li>
+      </template>
+      <li v-else>
+        Items
+      </li> -->
+      <!-- <template v-if="todos.lenght > 0">
+        <li v-for="(index, todo) in todos" :key="index">
+          {{ todo }}
+        </li>
+      </template>
+      <li v-else>No item</li> -->
+      <!-- <li v-for="(valor, chave, index) in todos" :key="chave">
+        {{ chave }} : {{ valor }} - {{ index }}
+      </li> -->
+      <!-- <li v-for="(item, index) in todos" :key="index">
+        {{ item }} {{index }}
+      </li> -->
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListItem from "./components/ListItem.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      // todos: ["1", "2", "3"],
+      // todos:{
+      //   item1: '1',
+      //   item2: '2',
+      //   item3: '3',
+      // }
+      // todos: [
+      //   { label: "Todo 1", isCompleted: true },
+      //   { label: "Todo 2", isCompleted: true },
+      //   { label: "Todo 3", isCompleted: false },
+      //   { label: "Todo 4", isCompleted: false },
+      // ],
+      todos: [
+        { label: "Item-1", value: 1 },
+        { label: "Item-2", value: 2 },
+        { label: "Item-3", value: 3 },
+        { label: "Item-4", value: 4 },
+        { label: "Item-5", value: 5 },
+      ],
+    };
+  },
+  components: { ListItem },
+  computed: {
+    filteredTodos() {
+      return this.todos.filter((todo) => !todo.isCompleted);
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>{{ label }} {{ value }}</li>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    // todo: Object
+    label: String,
+    value: [String, Number],
+  },
+};
+</script>
+
+<style></style>
